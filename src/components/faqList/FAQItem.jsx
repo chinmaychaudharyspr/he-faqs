4b51668dfcaa95aca289570af2d75dab1bf81123
Tirth Shah <tirth.shah@sprinklr.com>
2024-09-06 19:18:32 +0530

    Remove dark mode styles

--- src/components/faqList/FAQItem.jsx
+++ src/components/faqList/FAQItem.jsx
@@ -9,7 +9,7 @@ export const FAQItem = ({ item, onRemove, canRemove }) => {
       data-testid={`faq-${item.id}`}
       className=""
     >
-      <button onClick={() => setIsExpanded(true)} className={`flex items-center justify-between w-full p-5 font-medium rtl:text-right text-gray-500 border border-b-1 border-gray-200 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-800 dark:border-gray-700 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800 gap-3 rounded-t-xl ${isExpanded ? '' : 'rounded-b-xl'}`}>
+      <button onClick={() => setIsExpanded(true)} className={`flex items-center justify-between w-full p-5 font-medium rtl:text-right text-gray-500 border border-b-1 border-gray-200 focus:ring-4 focus:ring-gray-200 hover:bg-gray-100 gap-3 rounded-t-xl ${isExpanded ? '' : 'rounded-b-xl'}`}>
         <div data-testid="expand" className="flex-none pr-2">
             <svg data-accordion-icon className={`w-3 h-3 shrink-0 ${isExpanded ? '' : 'rotate-180'}`} aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
               <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5 5 1 1 5"/>
@@ -32,7 +32,7 @@ export const FAQItem = ({ item, onRemove, canRemove }) => {
           </div>
         ) : null}
       </button>
-      {isExpanded ? <div className="text-gray-500 dark:text-gray-400 p-5 border border-t-0 border-gray-200 dark:border-gray-700 dark:bg-gray-900 rounded-b-xl">{item.description}</div> : null}
+      {isExpanded ? <div className="text-gray-500 p-5 border border-t-0 border-gray-200 rounded-b-xl">{item.description}</div> : null}
     </div>
   );
 };
